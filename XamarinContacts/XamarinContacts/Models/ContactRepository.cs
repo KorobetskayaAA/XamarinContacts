d280482b384c96982d02c1973401fb2730df7f8d
A.Korobetskaya <kornast@yandex.ru>
2020-05-20 21:28:40 +0400

    Добавлен класс для управления БД

---
+++ XamarinContacts/XamarinContacts/Models/ContactRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SQLite;
+
+namespace XamarinContacts
+{
+    public class ContactRepository
+    {
+        SQLiteConnection database;
+
+        public ContactRepository(string databasePath)
+        {
+            database = new SQLiteConnection(databasePath);
+            database.CreateTable<Contact>();
+        }
+
+        public IEnumerable<Contact> GetContacts()
+        {
+            return database.Table<Contact>().ToList();
+        }
+
+        public Contact GetContact(int id)
+        {
+            return database.Get<Contact>(id);
+        }
+
+        public int DeleteContact(int id)
+        {
+            return database.Delete<Contact>(id);
+        }
+
+        public int SaveContact(Contact contact)
+        {
+            // у контакта есть id - значит он уже добавлен в БД
+            if (contact.Id != 0)
+            {
+                database.Update(contact);
+                return contact.Id;
+            }
+            // у нового контакта id нет - надо вставить новую запись
+            return database.Insert(contact);
+        }
+    }
+}
