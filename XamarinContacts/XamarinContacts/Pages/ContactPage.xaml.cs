93a53cd34de8f9d70ed32301635b4db165d8fa05
A.Korobetskaya <kornast@yandex.ru>
2020-05-20 21:30:01 +0400

    Добавлена страница редактирования контакта

---
+++ XamarinContacts/XamarinContacts/Pages/ContactPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinContacts
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContactPage : ContentPage
+    {
+        public ContactPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            Contact contact = (Contact)BindingContext;
+            App.Repository.SaveContact(contact);
+            Navigation.PopAsync();
+        }
+
+        private void DeleteButton_Clicked(object sender, EventArgs e)
+        {
+            Contact contact = (Contact)BindingContext;
+            App.Repository.DeleteContact(contact.Id);
+            Navigation.PopAsync();
+        }
+
+        private void CancelButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
