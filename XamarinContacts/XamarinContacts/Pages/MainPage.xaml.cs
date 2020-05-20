2cf0a0d7a1584d51f8b5a03cd8432b6c0f860daf
A.Korobetskaya <kornast@yandex.ru>
2020-05-20 21:30:21 +0400

    На главную страницу добавлен список контактов

--- XamarinContacts/XamarinContacts/Pages/MainPage.xaml.cs
+++ XamarinContacts/XamarinContacts/Pages/MainPage.xaml.cs
@@ -17,5 +17,27 @@ namespace XamarinContacts
         {
             InitializeComponent();
         }
+
+        protected override void OnAppearing()
+        {
+            BindingContext = App.Repository.GetContacts();
+            base.OnAppearing();
+        }
+
+        async void AddContact_Clicked(object sender, EventArgs e)
+        {
+            Contact contact = new Contact();
+            ContactPage cp = new ContactPage();
+            cp.BindingContext = contact;
+            await Navigation.PushAsync(cp);
+        }
+
+        async void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Contact contact = (Contact)e.SelectedItem;
+            ContactPage cp = new ContactPage();
+            cp.BindingContext = contact;
+            await Navigation.PushAsync(cp);
+        }
     }
 }
